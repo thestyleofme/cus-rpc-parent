d23d58ad364f27f331842bcbbd9c58cff2ffc3b3
isaac <codingdebugallday@163.com>
2020-10-16 02:29:34 +0800

    :bulb:

--- rmi/rmi-client/src/main/java/com/github/codingdebugallday/rmi/RmiClient.java
+++ rmi/rmi-client/src/main/java/com/github/codingdebugallday/rmi/RmiClient.java
@@ -21,7 +21,8 @@ public class RmiClient {
     public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
         // 从注册表中获取远程对象，强转
         HelloService helloService = (HelloService) Naming.lookup("//127.0.0.1:8888/rmiServer");
-        // 准备参数 这里的user必须跟服务端一致，包括serialVersionUID
+        // 准备参数 这里的user必须跟服务端一致
+        // RMI要求这两个类必须一致，包括包名和方法属性，甚至serialVersionUID
         User user = new User("isaac", 24);
         // 调用远程方法 sayHello
         String result = helloService.sayHello(user);
